2c05df61779e5a89fe25e575aa3616360535776f
yannemal <56878180+Yannemal@users.noreply.github.com>
2023-08-15 13:15:49 +0800

    2nd screen set up
    - GeometryReader
    - handson with PreviewProvider

--- moonShot/View/MissionView.swift
+++ moonShot/View/MissionView.swift
@@ -11,15 +11,39 @@ import SwiftUI
 
 struct MissionView: View {
     // MARK: - DATA Content
-    
+    let mission: Mission
     // init Class here :
     
     
     var body: some View {
         
-        Color.indigo
-            .ignoresSafeArea()
-            .opacity(0.7)
+        GeometryReader { geometry in
+            ScrollView {
+                VStack {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(maxWidth: geometry.size.width * 0.6)
+                        .padding(.top)
+                    
+                    VStack(alignment: .leading) {
+                        Text("Mission Highlights")
+                            .font(.title.bold())
+                            .padding(.bottom, 5)
+                        
+                        Text(mission.description)
+                    } // inner VStack
+                    .padding(.horizontal)
+                } // outer Vstack
+                .padding(.bottom)
+                // to help w iOS buttons on the bottom of the screen
+            }
+            
+        } // end outermost Stack
+        .navigationTitle(mission.displayName)
+        .navigationBarTitleDisplayMode(.inline)
+        // to get less chunky titles for NavStack
+        .background(.darkBackGround)
         
     } // end body some view THEN
     
@@ -33,9 +57,13 @@ struct MissionView: View {
 
 
 // MARK: - PREVIEW
+// needs more handson for preview gto work here
 struct MissionView_Previews: PreviewProvider {
+    static let missions: [Mission] = Bundle.main.decode("missions.json")
+    
     static var previews: some View {
-        MissionView()
+        MissionView(mission: missions[0])
+            .preferredColorScheme(.dark)
     }
 }
 
